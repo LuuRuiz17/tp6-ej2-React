d3c67ce66b950718a7cf365c4fec3545714a6a45
Luciana Ruiz <lucianaruiz.934@gmail.com>
2025-07-01 18:15:52 -0300

    Borré el useState ya que no lo estaba utilizando

--- src/App.jsx
+++ src/App.jsx
@@ -2,10 +2,9 @@ import 'bootstrap/dist/css/bootstrap.min.css';
 import { Container } from "react-bootstrap";
 import Saludo from "./components/Saludo";
 import 'animate.css';
-import { useState } from 'react';
 
 function App() {
-  const [saludar, setSaludar] = useState("My Friend");
+  const saludar = "My Friend";
   return (
     <Container className="d-flex justify-content-center align-items-center">
       <Saludo saludar = {saludar}></Saludo>
